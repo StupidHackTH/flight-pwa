fd92262b431b55b1ebb36b258a249675fd04fbef
Phiyawat Vongsawat <phiyawat9597@gmail.com>
2017-10-01 03:29:24 +0700

    get data in graph API on events

---
+++ src/components/Hello.vue
@@ -0,0 +1,88 @@
+<template>
+  <div class="hello">
+    <!--
+    <h1>{{ msg }}</h1>
+    <h2>Essential Links</h2>
+    <ul>
+      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
+      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
+      <li><a href="http://chat.vuejs.org/" target="_blank" rel="noopener">Vue Community Chat</a></li>
+      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
+      <li><a href="http://vuejs-templates.github.io/webpack/" target="_blank" rel="noopener">Docs for This Template</a></li>
+    </ul>
+    <h2>Ecosystem</h2>
+    <ul>
+      <li><a href="http://router.vuejs.org/" target="_blank" rel="noopener">vue-router</a></li>
+      <li><a href="http://vuex.vuejs.org/" target="_blank" rel="noopener">vuex</a></li>
+      <li><a href="http://vue-loader.vuejs.org/" target="_blank" rel="noopener">vue-loader</a></li>
+      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
+    </ul>
+    -->
+
+    <p> Find my event </p>
+
+    <fb-signin-button :params="fbSignInParams" @success="onSignInSuccess" @error="onSignInError">
+      Sign in with Facebook
+    </fb-signin-button>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'hello',
+  data () {
+    return {
+      msg: 'Welcome to My Vue.js PWA',
+      fbSignInParams: {
+        scope: 'email,user_likes',
+        return_scopes: true
+      }
+    }
+  },
+  methods: {
+    onSignInSuccess (response) {
+      // graph api in Facebook
+      window.FB.api('/me/events', text => {
+        // getdata in array[0] and getarrayinJSON
+        console.log(`Good to see you, ${text.data[0]['name']}`)
+      })
+    },
+    onSignInError (error) {
+      console.log('OH NO', error)
+    }
+  }
+}
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+  h1,
+  h2 {
+    font-weight: normal;
+  }
+  
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
+  
+  li {
+    display: inline-block;
+    margin: 0 10px;
+  }
+  
+  a {
+    color: #35495E;
+  }
+  
+  .fb-signin-button {
+    /* This is where you control how the button looks. Be creative! */
+    display: inline-block;
+    padding: 4px 8px;
+    border-radius: 3px;
+    background-color: #4267b2;
+    color: #fff;
+  }
+</style>
---
+++ src/main.js
@@ -0,0 +1,20 @@
+/* eslint-disable */
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import FBSignInButton from 'vue-facebook-signin-button'
+
+Vue.config.productionTip = false
+
+/* eslint-disable no-new  '282930508874277' */
+
+Vue.use(FBSignInButton)
+
+new Vue({
+  el: '#app',
+  router,
+  template: '<App/>',
+  components: {App}
+})
